860acec44aa2eb3eaecad9b6aee059f93c318323
David Horès <hores.d@gmail.com>
2026-04-15 16:57:30 +0200

    Update HomeController and home index and base templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findBy([], ['id' => 'DESC'], 9);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
