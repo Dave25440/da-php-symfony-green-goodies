97f7553716563062ba84bf8ea1fe5b737da1b51f
David Horès <hores.d@gmail.com>
2026-04-22 16:27:09 +0200

    Create route and add method in CartController and update product show template

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CartController extends AbstractController
 {
+    public function __construct(
+        private RequestStack $requestStack,
+    ) {}
+
     #[Route('/cart', name: 'app_cart', methods: ['GET'])]
     public function show(): Response
     {
         return $this->render('cart/show.html.twig', [
             'controller_name' => 'CartController',
         ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'app_cart_add', methods: ['GET'])]
+    public function add(Product $product): Response
+    {
+        $session = $this->requestStack->getSession();
+
+        $cart = $session->get('cart', []);
+        $order = $session->get('order', []);
+
+        $id = $product->getId();
+
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+            $order[] = $id;
+        }
+
+        $session->set('cart', $cart);
+        $session->set('order', $order);
+
+        return $this->redirectToRoute('app_cart');
     }
 }
