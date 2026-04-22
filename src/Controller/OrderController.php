3a257fc38472c760cb43ec3738d92eb012a88e8d
David Horès <hores.d@gmail.com>
2026-04-22 18:43:30 +0200

    Replace route and index method with new in OrderController and remove order index template

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Entity\OrderItem;
+use App\Service\CartService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private RequestStack $requestStack,
+    ) {}
+
+    #[Route('/order/new', name: 'app_order_new', methods: ['POST'])]
+    public function new(CartService $cartService): Response
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
-        ]);
+        $user = $this->getUser();
+        $cart = $cartService->getCart();
+
+        if (empty($cart['cartItems'])) {
+            $this->addFlash('warning', 'Votre panier est vide.');
+
+            return $this->redirectToRoute('app_cart');
+        }
+
+        $order = new Order();
+        $order->setDate(new \DateTimeImmutable());
+        $order->setTotal($cart['total']);
+        $order->setUser($user);
+
+        foreach ($cart['cartItems'] as $item) {
+            $orderItem = new OrderItem();
+            $orderItem->setQuantity($item['quantity']);
+            $orderItem->setPrice($item['product']->getPrice());
+            $orderItem->setProduct($item['product']);
+            $orderItem->setPurchase($order);
+
+            $order->addOrderItem($orderItem);
+        }
+
+        $this->manager->persist($order);
+        $this->manager->flush();
+
+        $session = $this->requestStack->getSession();
+        $session->remove('cart');
+        $session->remove('order');
+
+        $this->addFlash('success', 'Votre commande a été enregistrée.');
+
+        return $this->redirectToRoute('app_account');
     }
 }
