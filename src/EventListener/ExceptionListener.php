39a27fc5f099b2d1e4d401a26adaeee141fcbc78
David Horès <hores.d@gmail.com>
2026-04-23 12:12:58 +0200

    Create ExceptionListener and add it to services.yaml

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ExceptionListener
+{
+    /**
+     * Transforme les exceptions HTTP en réponse JSON.
+     *
+     * @param ExceptionEvent $event
+     * @return void
+     */
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpException) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage(),
+            ];
+
+            $event->setResponse(new JsonResponse($data));
+        }
+    }
+}
