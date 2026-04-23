09630ba0b7790848a38fc0039129f53ac59b0791
David Horès <hores.d@gmail.com>
2026-04-23 12:11:58 +0200

    Create AuthenticationSuccessListener and add it to services.yaml

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class AuthenticationSuccessListener
+{
+    /**
+     * Vérifie l'accès API de l'utilisateur authentifié.
+     *
+     * @param AuthenticationSuccessEvent $event
+     * @return void
+     */
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user || !in_array('ROLE_API', $user->getRoles(), true)) {
+            throw new HttpException(Response::HTTP_FORBIDDEN, 'Accès API désactivé.');
+        }
+    }
+}
