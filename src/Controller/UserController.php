3ee2156e6247859c4b3e7e80dfdba8cc35eeb802
David Horès <hores.d@gmail.com>
2026-04-23 23:01:57 +0200

    Create routes and enableApiAccess and disableApiAccess methods and update delete method in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,11 +8,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 final class UserController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $manager,
+        private TokenStorageInterface $tokenStorage,
     ) {}
 
     /**
@@ -35,6 +37,60 @@ final class UserController extends AbstractController
     }
 
     /**
+     * Active l'accès API de l'utilisateur authentifié.
+     *
+     * @return Response
+     */
+    #[Route('/account/api/enable', name: 'app_account_api_enable', methods: ['POST'])]
+    public function enableApiAccess(): Response
+    {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        $roles = $user->getRoles();
+
+        if (!in_array('ROLE_API', $roles, true)) {
+            $roles[] = 'ROLE_API';
+            $user->setRoles($roles);
+
+            $this->manager->persist($user);
+            $this->manager->flush();
+
+            $token = new UsernamePasswordToken($user, 'main', $roles);
+            $this->tokenStorage->setToken($token);
+        }
+
+        return $this->redirectToRoute('app_account');
+    }
+
+    /**
+     * Désactive l'accès API de l'utilisateur authentifié.
+     *
+     * @return Response
+     */
+    #[Route('/account/api/disable', name: 'app_account_api_disable', methods: ['POST'])]
+    public function disableApiAccess(): Response
+    {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        $roles = $user->getRoles();
+
+        if (in_array('ROLE_API', $roles, true)) {
+            $roles = array_diff($roles, ['ROLE_API']);
+            $user->setRoles($roles);
+
+            $this->manager->persist($user);
+            $this->manager->flush();
+
+            $token = new UsernamePasswordToken($user, 'main', $roles);
+            $this->tokenStorage->setToken($token);
+        }
+
+        return $this->redirectToRoute('app_account');
+    }
+
+    /**
      * Supprime le compte de l'utilisateur authentifié et invalide la session.
      *
      * @param TokenStorageInterface $tokenStorage
@@ -42,14 +98,14 @@ final class UserController extends AbstractController
      * @return Response
      */
     #[Route('/account/delete', name: 'app_account_delete', methods: ['DELETE'])]
-    public function delete(TokenStorageInterface $tokenStorage, Request $request): Response
+    public function delete(Request $request): Response
     {
         $user = $this->getUser();
 
         $this->manager->remove($user);
         $this->manager->flush();
 
-        $tokenStorage->setToken(null);
+        $this->tokenStorage->setToken(null);
         $request->getSession()->invalidate();
 
         return $this->redirectToRoute('app_home');
